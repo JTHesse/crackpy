2306de528c8760f48637806c5dd28e1710837ff6
Jan-Timo Hesse <81564892+JTHesse@users.noreply.github.com>
2023-01-13 11:31:03 +0100

    Update __init__.py

--- crackpy/__init__.py
+++ crackpy/__init__.py
@@ -1,7 +1,5 @@
 # imports from this package
 import crackpy.fracture_analysis
-import crackpy.crack_detection
-import crackpy.crack_detection.utils
 import crackpy.structure_elements
 
 # package information
